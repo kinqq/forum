69766454167f8503cc41c68b44c165ee60788635
kinqq <minkyu070602@gmail.com>
2021-03-20 13:28:39 +0900

    Updates

---
+++ precache-manifest.662f5235e942aa215f272c28956c7935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123be5052f7cc7e0649f7676a582a3be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4470ab9f8e56728d799c",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "960be74a5404f8f14012",
+    "url": "/static/css/main.44de4a75.chunk.css"
+  },
+  {
+    "revision": "4470ab9f8e56728d799c",
+    "url": "/static/js/2.8d609cb3.chunk.js"
+  },
+  {
+    "revision": "9d14d9350ffdb8132e7ebe92e3b039c5",
+    "url": "/static/js/2.8d609cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960be74a5404f8f14012",
+    "url": "/static/js/main.ecd3d87c.chunk.js"
+  },
+  {
+    "revision": "bd00b011b89de116e745",
+    "url": "/static/js/runtime-main.85d7b3f1.js"
+  }
+]);
